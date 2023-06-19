295af3c73251b77f5f244e6e87ea9851b15c376c
NeilClack <131825138+NeilClack@users.noreply.github.com>
2023-06-19 08:52:09 -0500

    Creaate search bar and pass location up to app.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import './App.css';
-import LocationSearchBar from './components/locationSearchBar';
+import { useState } from "react";
+import "./App.css";
+import LocationSearchBar from "./components/locationSearchBar";
 
 function App() {
+  const [location, setLocation] = useState("");
+
+  const formSubmitHandler = (submittedLocation) => {
+    setLocation(submittedLocation);
+    console.log(submittedLocation);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>My Weather</h1>
         <h2>Quickly Check your Local Weather</h2>
-        <p><sub><i>Powered by OpenWeather APIs</i></sub></p>
+        <p>
+          <sub>
+            <i>Powered by OpenWeather APIs</i>
+          </sub>
+        </p>
       </header>
       <div className="page-content">
-        <LocationSearchBar />
+        <LocationSearchBar onFormSubmit={formSubmitHandler} />
       </div>
     </div>
   );
--- src/components/locationSearchBar.js
+++ src/components/locationSearchBar.js
@@ -1,6 +1,30 @@
 import { useState } from "react";
 
-const LocationSearchBar = () => {
+const LocationSearchBar = (props) => {
+  const [enteredLocation, setEnteredLocation] = useState("");
+
+  // In some cases, updating like this could fail due to React state update scheduling.
+  // If several state updates are scheduled, you may end up updating a previous version of the state, rather than the "current".
+  // In this case, I am not depending on the previous state, so I will leave this as is.
+  // Here's how to do it if you depend on the previous state,
+  // and I'll use an object as the previous state to make it more obvious what is happening.
+  //
+  // setEnteredLocation((prevState) => {
+  //   return {..prevState, updatedKey: event.target.value};
+  // })
+  const locationChangeHandler = (event) => {
+    setEnteredLocation(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    // Prevents the form from pushing data to the server.
+    event.preventDefault();
+    const submittedLocation = enteredLocation;
+    props.onFormSubmit(submittedLocation);
+    // Reset the location field after submitting.
+    setEnteredLocation("");
+  };
+
   return (
     <div>
       <form>
@@ -8,9 +32,13 @@ const LocationSearchBar = () => {
           <label>Location</label>
           <input
             type="text"
+            value={enteredLocation}
             placeholder="Enter your city name or zipcode"
+            onChange={locationChangeHandler}
           ></input>
-          <button type="submit">Submit</button>
+          <button type="submit" onClick={submitHandler}>
+            Submit
+          </button>
         </fieldset>
       </form>
     </div>
