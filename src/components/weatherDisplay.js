e660edcb23da0bca424acc9e2bb9668584d5907f
NeilClack <131825138+NeilClack@users.noreply.github.com>
2023-07-22 21:00:38 -0500

    Update WeatherDisplay HTML tags

--- src/components/weatherDisplay.js
+++ src/components/weatherDisplay.js
@@ -1,6 +1,8 @@
 import { styled } from "styled-components";
 
-const Current = styled.div``;
+const WeatherCard = styled.div``;
+
+const WeatherIcon = styled.img``;
 
 const WeatherDisplay = (props) => {
   if (props.weatherData.current) {
@@ -9,22 +11,29 @@ const WeatherDisplay = (props) => {
         <div>
           <h2>{props.location}</h2>
         </div>
-        <Current>
-          <p>Current Condition: {props.weatherData.current.weather[0].main}</p>
-          <p>{props.weatherData.current.weather[0].description}</p>
-          <p>Sunrise: {props.weatherData.current.sunrise}</p>
-          <p>Sunset: {props.weatherData.current.sunset}</p>
-          <p>Temperature: {props.weatherData.current.temp}</p>
-          <p>Feels Like: {props.weatherData.current.feels_like}</p>
-          <p>Barometric Pressure: {props.weatherData.current.pressure}</p>
-          <p>Humidity: {props.weatherData.current.humidity}</p>
-          <p>Dew Point: {props.weatherData.current.dew_point}</p>
-          <p>UV Index: {props.weatherData.current.uvi}</p>
-          <p>Clouds: {props.weatherData.current.clouds}</p>
-          <p>Visibility: {props.weatherData.current.visibility}</p>
-          <p>Wind Speed: {props.weatherData.current.wind_speed}</p>
-          <p>Wind Direction: {props.weatherData.current.wind_deg}</p>
-        </Current>
+        <WeatherCard>
+          <h2>
+            Current Conditions: {props.weatherData.current.weather[0].main}
+          </h2>
+          <h3>{props.weatherData.current.weather[0].description}</h3>
+          <WeatherIcon
+            src={`https://openweathermap.org/img/wn/${props.weatherData.current.weather[0].icon}@2x.png`}
+          />
+          <div>
+            <p>Sunrise: {props.weatherData.current.sunrise}</p>
+            <p>Sunset: {props.weatherData.current.sunset}</p>
+            <p>Temperature: {props.weatherData.current.temp}</p>
+            <p>Feels Like: {props.weatherData.current.feels_like}</p>
+            <p>Barometric Pressure: {props.weatherData.current.pressure}</p>
+            <p>Humidity: {props.weatherData.current.humidity}</p>
+            <p>Dew Point: {props.weatherData.current.dew_point}</p>
+            <p>UV Index: {props.weatherData.current.uvi}</p>
+            <p>Clouds: {props.weatherData.current.clouds}</p>
+            <p>Visibility: {props.weatherData.current.visibility}</p>
+            <p>Wind Speed: {props.weatherData.current.wind_speed}</p>
+            <p>Wind Direction: {props.weatherData.current.wind_deg}</p>
+          </div>
+        </WeatherCard>
       </div>
     );
   } else {
