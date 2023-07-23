d63da778b4e179ad2c5af16fe0a5b8f30d94a38b
NeilClack <131825138+NeilClack@users.noreply.github.com>
2023-07-22 20:40:06 -0500

    Update units to Imperial

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,20 +1,20 @@
 export async function geoQuery(enteredLocation) {
-    try {
-      const res = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${enteredLocation}&limit=5&appid=${process.env.REACT_APP_OW_API_KEY}`
-      );
-      const data = await res.json();
-      return data;
-    } catch (err) {
-      console.error(err);
-    }
+  try {
+    const res = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${enteredLocation}&limit=5&appid=${process.env.REACT_APP_OW_API_KEY}`
+    );
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 export const weatherQuery = async (lat, lon) => {
   try {
     const res = await fetch(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OW_API_KEY}`
-    )
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${process.env.REACT_APP_OW_API_KEY}`
+    );
     const data = await res.json();
     console.log("from query");
     console.log(data);
