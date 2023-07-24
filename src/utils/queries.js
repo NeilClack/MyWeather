f8ce5268fbf52321e164fb7eb7f0a66950bc39b2
NeilClack <131825138+NeilClack@users.noreply.github.com>
2023-07-23 21:47:44 -0500

    Modify geocoding api to utilize https

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,7 +1,7 @@
 export async function geoQuery(enteredLocation) {
   try {
     const res = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${enteredLocation}&limit=5&appid=${process.env.REACT_APP_OW_API_KEY}`
+      `https://api.openweathermap.org/geo/1.0/direct?q=${enteredLocation}&limit=5&appid=${process.env.REACT_APP_OW_API_KEY}`
     );
     const data = await res.json();
     return data;
