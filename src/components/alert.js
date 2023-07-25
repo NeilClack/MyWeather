0fa276f7a01c89cdf0e9f45d65d9245999919206
NeilClack <131825138+NeilClack@users.noreply.github.com>
2023-07-24 21:04:08 -0500

    Create Alert component to display weather alerts

---
+++ src/components/alert.js
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+import { convertTsToTime } from "../utils/datetime";
+
+const Alert = (props) => {
+  return (
+    <div>
+      <p>{props.alert.sender}</p>
+      <p>{props.alert.event}</p>
+      <p>Start: {convertTsToTime(props.alert.start)}</p>
+      <p>End: {convertTsToTime(props.alert.end)}</p>
+      <p>{props.alert.description}</p>
+    </div>
+  );
+};
+
+export default Alert;
