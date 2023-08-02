18d847966e3920dc37bf73f79e545941e72f3b14
NeilClack <131825138+NeilClack@users.noreply.github.com>
2023-08-02 10:36:28 -0500

    Correct timestamp calculation

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -1,9 +1,7 @@
 const convertTsToTime = (timestamp) => {
-  let tempDate = new Date(timestamp * 1000);
-
+  let tempDate = new Date(timestamp);
   let hours = tempDate.getHours();
   let minutes = tempDate.getMinutes();
-
   let convertedTime = `${hours}:${minutes}`;
 
   return convertedTime;
